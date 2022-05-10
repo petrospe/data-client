9138ecac0c995869a9e8fe8f2aa5124f017516e4
petrospe <petros_pe@yahoo.gr>
2022-05-10 22:16:12 +0300

    first commit

---
+++ components/ProductCard.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="md:flex">
+        <div class="md:flex-shrink-0">
+            <img class="rounded-lg md:w56" :src="product.image" :alt="product.title" />
+        </div>
+        <div class="mt4 md:mt-0 md:ml-6">
+            <div class="uppercase tracking-wide text-sm text-indigo-600 font-bold">
+                {{ product.continent }}
+            </div>
+            <NuxtLink
+            :to="product.slug"
+            class="block mt-1 text-lg leading-tight font-semibold text-gray-900 hover:underline">
+            </NuxtLink>
+            <p class="mt-2 text-gray-600">
+                {{ product.description }}
+            </p>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    props: {
+        product: {
+            type: Object,
+            default: () => {}
+        }
+    },
+}
+</script>
---
+++ pages/products.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="container mx-auto mt-8">
+        <ProductCard
+         v-for="mountain in mountains" 
+         :product="mountain" 
+         :key="mountain.title" 
+         class="mb-4"
+        />
+    </div>
+</template>
+<script>
+export default {
+    async fetch() {
+      this.mountains = await fetch(
+        'https://api.nuxtjs.dev/mountains'
+      ).then(res => res.json())
+    },
+    data() {
+        return {
+            mountains:[]
+        }
+    },
+    
+}
+</script>
+<style lang="postcss">
+img {
+    height: 120px;
+}
+</style>
